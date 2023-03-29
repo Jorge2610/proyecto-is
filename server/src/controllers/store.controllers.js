ec76bcefb7b6dcdd85597dcf9e9135aaa5cd9667
RodrigoFlo666 <rodrigofloresv666@gmail.com>
2023-03-29 00:27:20 -0400

    crear y visualizar productos-lotes

--- server/src/controllers/store.controllers.js
+++ server/src/controllers/store.controllers.js
@@ -1,42 +1,108 @@
-const pool = require('../db')
+const pool = require("../db");
 
 const getAllCategories = async (req, res) => {
-    const result = await pool.query('SELECT * FROM categorias')
-    console.log(result)
-    res.json(result.rows)
-}
+  const result = await pool.query("SELECT * FROM categorias");
+  console.log(result);
+  res.json(result.rows);
+};
 
 const getACategorie = async (req, res) => {
-    const result = await pool.query('SELECT * FROM categorias')
-    console.log(result)
-    res.json(result.rows[0])
-}
+  const result = await pool.query("SELECT * FROM categorias");
+  console.log(result);
+  res.json(result.rows[0]);
+};
 
 const createACategorie = async (req, res) => {
-    const { name } = req.body
+  const { name } = req.body;
 
-    try {
-        const result = await pool.query('INSERT INTO categorias (nombre_categoria) VALUES ($1)', [name]);
-        
-        res.json(result.rows[0]);
-    } catch (error) {
-        console.log("El nombre de la categoria ya existe!")
-        res.json({error: error.message});
-    }
-}
+  try {
+    const result = await pool.query(
+      "INSERT INTO categorias (nombre_categoria) VALUES ($1)",
+      [name]
+    );
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.log("El nombre de la categoria ya existe!");
+    res.json({ error: error.message });
+  }
+};
 
 const deleteACategorie = (req, res) => {
-    res.send('Borramos una categoria')
-}
+  res.send("Borramos una categoria");
+};
 
 const updateACategorie = (req, res) => {
-    res.send('Actualizamos una categoria');
-}
+  res.send("Actualizamos una categoria");
+};
+//////PRODUCTOS/////////////////
+
+const getAllProductsLotes = async (req, res) => {
+  const result = await pool.query("SELECT * FROM productos,lotes");
+
+  console.log(result);
+
+  res.json(result.rows);
+};
+
+const createProduct = async (req, res) => {
+  try {
+    const { id_product, name_product, id_category, description_product } =
+      req.body;
+    const newProduct = await pool.query(
+      "INSERT INTO productos (id_producto, nombre_producto, id_categoria, descripcion) VALUES($1, $2, $3, $4) RETURNING *",
+      [id_product, name_product, id_category, description_product]
+    );
+
+    res.json(newProduct.rows[0]);
+    //res.send("Creamos un producto");
+  } catch (error) {
+    console.log("El producto ya existe!");
+    res.json({ error: error.message });
+  }
+};
+
+const createLot = async (req, res) => {
+  try {
+    const {
+      id_lot,
+      id_product,
+      quantity,
+      unit_cost,
+      unit_price,
+      expiration_date,
+    } = req.body;
+    const newLot = await pool.query(
+      "INSERT INTO lotes (id_lote, id_producto, cantidad, costo_unitario, precio_unitario, fecha_caducidad) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [id_lot, id_product, quantity, unit_cost, unit_price, expiration_date]
+    );
+
+    res.json(newLot.rows[0]);
+    //res.send("Creamos un lote");
+  } catch (error) {
+    console.log("El lote del producto ya existe!");
+    res.json({ error: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  res.send("Eliminamos un producto");
+};
+
+const updateProduct = async (req, res) => {
+  res.send("Actualizamos un producto");
+};
 
 module.exports = {
-    getAllCategories,
-    getACategorie,
-    createACategorie,
-    deleteACategorie,
-    updateACategorie
-}       
+  getAllCategories,
+  getACategorie,
+  createACategorie,
+  deleteACategorie,
+  updateACategorie,
+
+  getAllProductsLotes,
+  createProduct,
+  createLot,
+  deleteProduct,
+  updateProduct,
+};
--- server/src/routes/store.routes.js
+++ server/src/routes/store.routes.js
@@ -1,26 +1,50 @@
-const {Router} = require('express');
-const pool = require('../db')
+const { Router } = require("express");
+const pool = require("../db");
 const router = Router();
-const {getAllCategories, getACategorie, createACategorie, deleteACategorie, updateACategorie} = require('../controllers/store.controllers')
+const {
+  getAllCategories,
+  getACategorie,
+  createACategorie,
+  deleteACategorie,
+  updateACategorie,
+  getAllProductsLotes,
+  createProduct,
+  createLot,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/store.controllers");
+
 //Ruta por defecto raiz.
-router.get('/', (req, res) => {
-    res.send('Hello World'); 
-})
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 //Devuelve todas las categorias
-router.get('/store/categories', getAllCategories)
+router.get("/store/categories", getAllCategories);
 
 //Devuelve una categoria en base al id. Esta en construccion
-router.get('/store/categories/1', getACategorie)
+router.get("/store/categories/1", getACategorie);
 
 //Creamos una categoria
-router.post('/store/categories/create', createACategorie)
+router.post("/store/categories/create", createACategorie);
 
 //Borramos una categoria
-router.delete('/store/categories/delete', deleteACategorie)
+router.delete("/store/categories/delete", deleteACategorie);
 
 //Actualizamos una categoria
-router.put('/store/categories/update', updateACategorie);
+router.put("/store/categories/update", updateACategorie);
 
+//Devuelve todos los productos
+router.get("/store/products", getAllProductsLotes);
+//Devuelve un producto
+//router.get('/store/products/1', getProduct)
+//Creamos un producto
+router.post("/store/products/create", createProduct);
+//creamos un lote
+router.post("/store/products/createLot", createLot);
+//Borramos un producto
+router.delete("/store/products/delete", deleteProduct);
+//Actualizamos un producto
+router.put("/store/products/update", updateProduct);
 
 module.exports = router;
