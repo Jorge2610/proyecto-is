61c0dcf583fa58ab6adbeafc442c9b19018fcf79
jesux28 <202002524@est.umss.edu>
2023-03-28 22:33:52 -0400

    Form con verificacion

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,5 +1,5 @@
 import './Form.css'
-import { PlusOutlined } from '@ant-design/icons';
+import { UploadOutlined } from '@ant-design/icons';
 import {
   Button,
   DatePicker,
@@ -9,14 +9,11 @@ import {
   Upload,
 } from 'antd';
 import { useState } from 'react';
+import moment from 'moment';
+import ModalConf from './ModalConfirmacion.js'
 
 const { RangePicker } = DatePicker;
 const { TextArea } = Input;
-
-function getDatos(){
-  console.log('El boton realiza algo');
-}
-
 
 const FormDisabledDemo = () => {
   return (
@@ -34,81 +31,75 @@ const FormDisabledDemo = () => {
           maxWidth: 600,
         }}
         className="form-producto"
-      > 
+      >
 
         <div className='botonx'>
-          <Button onClick={console.log(1)}>X</Button> 
+          <Button onClick={console.log(1)}>X</Button>
         </div>
-        <p >Añadir Producto</p> 
+        <p >Añadir Producto</p>
 
-        <Form.Item 
-              label="Subir Imagen" 
-              valuePropName="fileList"
+        <Form.Item
+          label="Subir Imagen"
+          valuePropName="fileList"
         >
-          
-            <Upload action="/upload.do" listType="picture-card">
-                <div>
-                  <PlusOutlined />
-                  <div
-                    style={{
-                      marginTop: 8,
-                    }}
-                  >
-                    Upload
-                  </div>
-                </div>
-            </Upload>
+
+          <Upload action="/upload.do" listType="picture"
+            accept="image/png, image/jpeg"
+            maxCount={1}
+          >
+            <Button icon={<UploadOutlined />}>Upload</Button>
+
+          </Upload>
         </Form.Item>
 
-        <Form.Item 
-            label="Nombre del Producto" 
-            className='labels' 
-            validateStatus="error" 
-            help="Should be combination of numbers & alphabets"
-        >
-            <Input htmlType='text' placeholder='Inserte nombre del Producto' className='controls' required/>
-        </Form.Item>  
-                <br></br>
-          
+        <Form.Item label="Nombre del Producto" className='labels' >
+          <Input htmlType='text' placeholder='Inserte nombre del Producto' className='controls' required />
+        </Form.Item>
+        <br></br>
+
         <Form.Item label="Cantidad" className='labels'>
-          <Input htmlType="number" placeholder='Inserte cantidad' min='1' className='controls' required/>
+          <Input type="number" placeholder='Inserte cantidad' min='1' className='controls' required />
         </Form.Item>
-                <br></br>
+        <br></br>
 
-        <Form.Item label="Costo Unitario" className='labels'> 
-            <Input htmlType="number" placeholder='Inserte costo unitario' min='0.0' className='controls' required/>
+        <Form.Item label="Costo Unitario" className='labels'>
+          <Input type="number" placeholder='Inserte costo unitario' min='1.0' className='controls' required />
         </Form.Item>
         <br></br>
         <Form.Item label="Precio" className='labels'>
-          <Input htmlType="number" className='controls' placeholder='Inserte el precio' required/>
+          <Input type="number" className='controls' placeholder='Inserte el precio' min='0.0' required />
         </Form.Item>
 
         <br></br>
         <Form.Item label="Seleccionar Categoria" className='categoria'>
           <Select placeholder='Seleccione la categoría'>
             {/**Recuperar de la BD las categorias**/}
-            
+
           </Select>
         </Form.Item>
-      
+
         <Form.Item label="Fecha de Caducidad " className='labels'>
-            <DatePicker className='controls' placeholder='Inserte la fecha'/>
+          <DatePicker className='controls' placeholder='Inserte la fecha'
+            disabledDate={(current) => {
+              return moment().add(-1, 'days') >= current;
+            }}
+          />
         </Form.Item>
-        
+
         <br></br>
-        
+
         <Form.Item label="Descripcion" className='labels'>
-            <TextArea rows={3} className='controls'/>
+          <TextArea rows={3} className='controls' />
         </Form.Item>
 
         <br></br>
         <br></br>
-        
+
         <Form.Item >
           <br></br>
           <br></br>
-          
-          <Button htmlType='submit'>Crear Producto</Button>
+
+          <Button htmlType='submit' onclick={ModalConf.showModal}>Crear Producto</Button>
         </Form.Item>
       </Form>
     </>
